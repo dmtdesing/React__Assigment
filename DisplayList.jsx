3833216fc7f25c2bd4996fe2f76beeaad15e63e4
dmtdesing <130656023+dmtdesing@users.noreply.github.com>
2023-05-24 20:30:48 +0300

    Add files via upload

---
+++ DisplayList.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import './DisplayList.css';
+
+const DisplayList = () => {
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (name !== "" && list !== "") {
+      setList([name,...list]);
+      setName('');
+    }
+  };
+
+  return (
+    <div className="displayList__wrapper">
+      <form>
+        <input
+          name="name"
+          type="text"
+          placeholder="Enter Your Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          
+        />
+        <button onClick={handleSubmit}> Add </button>
+      </form>
+
+      {list.map((ls) => (
+        <li>{ls}</li>
+      ))}
+    </div>
+  );
+};
+
+export default DisplayList;
